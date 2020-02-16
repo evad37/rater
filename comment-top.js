e5ccc58ddcc6006304f86a719fd123e7a0a54701
Evad37 <evad37@gmail.com>
2020-02-16 08:36:47 +0800

    Add build step to concat comments to minimised output

---
+++ comment-top.js
@@ -0,0 +1,14 @@
+/*
+ * Rater: dialog interface to add, remove, or modify WikiProject banners
+ * Author: Evad37
+ * Licence: MIT / CC-BY 4.0 [https://github.com/evad37/rater/blob/master/LICENSE]
+ * 
+ * Built from source code at GitHub repository [https://github.com/evad37/rater].
+ * All changes should be made in the repository, otherwise they will be lost.
+ * 
+ * To update this script from github, you must have a local repository set up. Then
+ * follow the instructions at [https://github.com/evad37/rater/blob/master/README.md]
+ */
+/* jshint esversion: 5, laxbreak: true, undef: true, eqnull: true, maxerr: 3000 */
+/* globals console, document, window, $, mw, OO, extraJs */
+/* <nowiki> */
